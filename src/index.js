647c555a220b43e7646502f48bb702dfe2a64bc5
yuhan222 <113611597+yuhan222@users.noreply.github.com>
2025-11-18 22:42:17 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AItest from './AItest';
-import { createRoot } from 'react-dom/client';
-
+import FooterWeather from './FooterWeather'; // ⭐ 新增這一行
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const el = document.getElementById('react-root');
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AItest />        
+    <FooterWeather /> {/* ⭐ 天氣會顯示在最下面 */}
   </React.StrictMode>
 );
 
-if (el) {
-  createRoot(el).render(<AItest />);
-}
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-
-
